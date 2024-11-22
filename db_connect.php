265c69f525060f9c7b0c6ec34d36ea4361dfb685
George winsome <georgechipaul@gmail.com>
2024-11-22 03:33:58 +0100

    reg app updated

--- db_connect.php
+++ db_connect.php
@@ -6,13 +6,3 @@ if (!$con) {
     die(mysqli_error($con));
 };
 
-
-// $con = new mysqli('localhost', 'paradise_registration', '7MC%xc3?fmq1', 'paradise_regform');
-
-// if (!$con) {
-//     die(mysqli_error($con));
-// };
-// U!CFjPw0QeJg
-// email: s8Om!_%vOZnc
-
-//database user:7MC%xc3?fmq1
